08406494de3baa399cd84b524a722591e6f3aa0a
guswnsdldl <guswnsdldl@naver.com>
2024-10-24 13:35:18 +0900

    ohj

--- src/main/java/com/app/ggumteo/domain/work/WorkVO.java
+++ src/main/java/com/app/ggumteo/domain/work/WorkVO.java
@@ -16,7 +16,7 @@ public class WorkVO {
     private Long id; // 작품 ID
     private String workPrice; // 작품 가격
     private Long genreId; // 장르 ID
-    private Integer readCount; // 조회수
+    private int readCount; // 조회수
     private String createdDate; // 생성 날짜
     private String updatedDate; // 수정 날짜
 
