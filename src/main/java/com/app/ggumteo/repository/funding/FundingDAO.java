6886e5af113b801cfea463c18c7423225ab7632d
000jisoo <000jisoo@gmail.com>
2024-10-25 09:36:58 +0900

    myfunding

--- src/main/java/com/app/ggumteo/repository/funding/FundingDAO.java
+++ src/main/java/com/app/ggumteo/repository/funding/FundingDAO.java
@@ -1,4 +1,14 @@
 package com.app.ggumteo.repository.funding;
 
+import com.app.ggumteo.mapper.funding.FundingMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class FundingDAO {
+    private final FundingMapper fundingMapper;
+
+//    내 게시물 조회
+    public List<>
 }
