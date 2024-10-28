99f5514cda2c03e86445d4a937c5f38768fc97b3
000jisoo <000jisoo@gmail.com>
2024-10-28 15:54:42 +0900

    myPage

--- src/main/resources/sql/funding/buy_funding_product.sql
+++ src/main/resources/sql/funding/buy_funding_product.sql
@@ -8,3 +8,15 @@ create table tbl_buy_funding_product (
                                          constraint fk_buy_funding_product_funding_product foreign key (funding_product_id)
                                              references tbl_funding_product (id)
 );
+
+select id, product_name, product_price, product_amount, funding_id
+from tbl_funding_product;
+insert into tbl_funding_product
+(product_name, product_price, product_amount, funding_id)
+values ('펀딩 상품 이름 test1', '50000', );
+
+select id, member_id, funding_product_id, funding_send_status
+from tbl_buy_funding_product;
+insert into tbl_buy_funding_product
+(member_id, funding_product_id, funding_send_status)
+values ();
