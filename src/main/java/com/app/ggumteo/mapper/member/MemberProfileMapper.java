0567480627411cf29494fa1c92b7d2662a2557f6
ChoiDh05 <topaz7107@gmail.com>
2024-10-31 09:02:25 +0900

    회원가입완료

--- src/main/java/com/app/ggumteo/mapper/member/MemberProfileMapper.java
+++ src/main/java/com/app/ggumteo/mapper/member/MemberProfileMapper.java
@@ -2,8 +2,10 @@ package com.app.ggumteo.mapper.member;
 
 import com.app.ggumteo.domain.member.MemberProfileVO;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface MemberProfileMapper {
-    public void insert(MemberProfileVO memberProfileVO);
+    // 새로운 회원 프로필 삽입 및 자동 생성된 ID 반환
+    void insert(MemberProfileVO memberProfileVO);
 }
--- src/main/java/com/app/ggumteo/service/file/ProfileFileService.java
+++ src/main/java/com/app/ggumteo/service/file/ProfileFileService.java
@@ -1,12 +1,11 @@
 package com.app.ggumteo.service.file;
 
-import com.app.ggumteo.domain.file.FileVO;
-
+import com.app.ggumteo.domain.file.ProfileFileDTO;
 import com.app.ggumteo.domain.file.ProfileFileVO;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface ProfileFileService {
     void saveProfileFile(ProfileFileVO profileFileVO);
-    // MultipartFile을 받아서 FileVO를 반환하는 메서드
-    FileVO saveProFile(MultipartFile file, Long memberProfileId);
+    // 변경: MultipartFile을 받아서 ProfileFileDTO를 반환하는 메서드
+    ProfileFileDTO saveProFile(MultipartFile file, Long memberProfileId);
 }
