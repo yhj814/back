50309cc6d79b55b67c7ee85e6c75168a5d67b7cd
guswnsdldl <guswnsdldl@naver.com>
2024-11-04 16:20:45 +0900

    목록 수정완료

--- src/main/resources/static/js/text/list.js
+++ src/main/resources/static/js/text/list.js
@@ -1,11 +1,11 @@
-// // 검색 기능: genreType을 빈 문자열로 설정하여 전체 검색
-// function searchWorks() {
-//     const keyword = document.getElementById('searchKeyword').value.trim();
-//     document.getElementById('genreType').value = "";  // genreType을 빈 문자열로 설정
-//
-//     const url = `/text/list?page=1&keyword=${encodeURIComponent(keyword)}`;
-//     window.location.href = url;
-// }
+// 검색 기능: genreType을 빈 문자열로 설정하여 전체 검색
+function searchWorks() {
+    const keyword = document.getElementById('searchKeyword').value.trim();
+    document.getElementById('genreType').value = "";  // genreType을 빈 문자열로 설정
+
+    const url = `/text/list?page=1&keyword=${encodeURIComponent(keyword)}`;
+    window.location.href = url;
+}
 //
 // // 장르 필터 적용 함수
 // function filterByGenre(genre) {
