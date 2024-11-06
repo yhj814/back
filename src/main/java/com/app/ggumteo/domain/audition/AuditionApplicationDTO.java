59961e9a1ee3e0cea19bbe899be99027fc55b4ec
ChoiDh05 <topaz7107@gmail.com>
2024-11-06 12:28:12 +0900

    css수정 및 신청페이지 돌입

---
+++ src/main/java/com/app/ggumteo/domain/audition/AuditionApplicationDTO.java
@@ -0,0 +1,32 @@
+package com.app.ggumteo.domain.audition;
+
+import lombok.*;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuditionApplicationDTO {
+    @EqualsAndHashCode.Include
+    private Long id;
+    private Long memberId;
+    private String applyEtc;
+    private Long auditionId;
+    private String confirmStatus;
+    private Long memberProfileId;
+    private String profileName;
+    private String profileGender;
+    private String profileAge;
+    private String profileEmail;
+    private String profilePhone;
+    private Long fileId;
+    private String fileName;
+    private String fileType;
+    private String fileSize;
+    private String filePath;
+    private String createdDate;
+    private String updatedDate;
+}
---
+++ src/main/java/com/app/ggumteo/service/audition/AuditionApplicationServiceImpl.java
@@ -0,0 +1,5 @@
+package com.app.ggumteo.service.audition;
+
+public class AuditionApplicationServiceImpl {
+
+}
