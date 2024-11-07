fe9a6b6f074870fa632c60b0e7baf01cdd51a76d
000jisoo <000jisoo@gmail.com>
2024-11-07 09:44:13 +0900

    service

--- src/main/resources/static/js/my/video/my-video-service.js
+++ src/main/resources/static/js/my/video/my-video-service.js
@@ -91,14 +91,14 @@ const myPageService = (() => {
         }
     }
 
-    // const upload = async (formDate) => {
-    //     const response = await fetch("/member/video/file/upload", {
-    //         method: "post",
-    //         body: formDate
-    //     });
-    //     const fileName = await response.json();
-    //     return fileName;
-    // }
+    const upload = async (formDate) => {
+        const response = await fetch("/member/video/file/upload", {
+            method: "post",
+            body: formDate
+        });
+        const fileName = await response.json();
+        return fileName;
+    }
 
     return {
         getMyVideoWorkList: getMyVideoWorkList,
