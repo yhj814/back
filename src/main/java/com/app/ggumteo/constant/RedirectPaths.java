def503f35346343092d3e80ab7a3f115f4097c95
ChoiDh05 <topaz7107@gmail.com>
2024-11-13 16:18:36 +0900

    알람

---
+++ src/main/java/com/app/ggumteo/constant/RedirectPaths.java
@@ -0,0 +1,9 @@
+package com.app.ggumteo.constant;
+
+public class RedirectPaths {
+    public static final String AUDITION_DETAIL = "redirect:/audition/text/detail/";
+    public static final String AUDITION_WRITE = "/audition/text/write";
+    public static final String LOGIN = "/main";
+    public static final String ERROR = "/error";
+    public static final String AUDITION_TEXT_ERROR = "/audition/text/error";
+}
--- src/main/java/com/app/ggumteo/service/audition/AuditionApplicationService.java
+++ src/main/java/com/app/ggumteo/service/audition/AuditionApplicationService.java
@@ -1,12 +1,13 @@
 package com.app.ggumteo.service.audition;
 
+import com.app.ggumteo.constant.AlarmSubType;
 import com.app.ggumteo.domain.audition.AuditionApplicationDTO;
 import com.app.ggumteo.domain.audition.AuditionApplicationVO;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface AuditionApplicationService {
 
-    void write(AuditionApplicationDTO auditionApplicationDTO);
+    void write(AuditionApplicationDTO auditionApplicationDTO, AlarmSubType alarmSubType);
 
     int countApplicantsByAuditionId(Long auditionId);
 }
