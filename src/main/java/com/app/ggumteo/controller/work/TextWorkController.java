870ace1cf564bcd3958bab542eb9e8efe521df73
ChoiDh05 <topaz7107@gmail.com>
2024-11-14 12:20:50 +0900

    funding alarm start

--- src/main/java/com/app/ggumteo/controller/work/TextWorkController.java
+++ src/main/java/com/app/ggumteo/controller/work/TextWorkController.java
@@ -253,6 +253,7 @@ public class TextWorkController {
 
         return "text/detail";
     }
+
     @PostMapping("/order")
     public ResponseEntity<String> completePayment(@RequestBody Map<String, Object> paymentData) {
         try {
@@ -298,7 +299,7 @@ public class TextWorkController {
             Long buyWorkId = savedBuyWork.getId(); // 저장된 구매 ID 추출
 
             // 알림 메시지 설정
-            String message = "새로운 작품이 구매되었습니다.";
+            String message = "등록한 작품이 구매되었습니다.";
 
             // AlarmService를 통해 알림 생성 (작품 작성자의 memberProfileId 사용)
             alarmService.createWorkAlarm(authorMemberProfileId, buyWorkId, message, AlarmSubType.TEXT);
